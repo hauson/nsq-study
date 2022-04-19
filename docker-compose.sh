1102ad8d002fbf2602a09b8286379968871f76dc
崔浩鑫 <cuihaoxin@cuihaoxindeMacBook-Pro.local>
2022-04-19 18:31:42 +0800

    feat(modify): modify

--- docker-compose.sh
+++ docker-compose.sh
@@ -2,7 +2,7 @@
 -f, --file 指定docker-compse 使用的文件
 --verbose 输出更多调式信息
 
-docker-compose up -d #启动nsq 所有组件
+docker-compose up -d #启动nsq 所有组件 --force-recreate 强制重新创建容器
 docker-compose down  #关闭nsq 所有组件
 docker-compose exec 进入制定的容器
 docker-compose kill 通过发送SIGKILL 信号来强制停止服务容器
@@ -21,11 +21,18 @@ docker-compose logs #查看组件日志
 docker-compose port 打印某个容器端口所映射的公共端口
 docker-compose top
 
-docker-compose 检查当前目录中docker-compose.yaml 文件格式是否正确
-docker-compose -f aa.yaml 检查指定文件的格式
+docker-compose config 检查当前目录中docker-compose.yaml 文件格式是否正确
+docker-compose config -f aa.yaml 检查指定文件的格式
 
 docker-compose build 构建所有组件的镜像
 
 # 删除docker-compose目录下的所有镜像
 docker-compose images | awk 'NR>=3 {print $4}' | xargs docker rmi
+
+docker-compose 配置项:
+services 的网络隔离 networks:
+services 启动顺序 depends_on:
+services 的数据卷 volumes: - 宿主机目录：容器目录
+services 的日志驱动   logging:
+
 
